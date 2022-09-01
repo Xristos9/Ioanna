1f20a3b6fadbf860ca30f45681a528662b34db92
Xristos9 <sonic9495@gmail.com>
2022-09-01 15:14:58 +0300

    remove crap from head

--- adminCharts.php
+++ adminCharts.php
@@ -5,20 +5,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<title>Home</title>
-	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-		integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-		crossorigin="" />
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" href="Leaflet.AnimatedSearchBox.css">
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-	<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-		integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-		crossorigin=""></script>
-	<script src="https://cdn.jsdelivr.net/npm/fuse.js@5.0.10-beta/dist/fuse.min.js"></script>
-	<script src="Leaflet.AnimatedSearchBox.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js" integrity="sha512-TW5s0IT/IppJtu76UbysrBH9Hy/5X41OTAbQuffZFU6lQ1rdcLHzpU5BzVvr/YFykoiMYZVWlr/PX1mDcfM9Qg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+	<title>Charts</title>
 </head>
 <body>
 	<div class="scrollmenu">
--- adminInfo.php
+++ adminInfo.php
@@ -5,19 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<title>Home</title>
-	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-		integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-		crossorigin="" />
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" href="Leaflet.AnimatedSearchBox.css">
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-	<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-		integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-		crossorigin=""></script>
-	<script src="https://cdn.jsdelivr.net/npm/fuse.js@5.0.10-beta/dist/fuse.min.js"></script>
-	<script src="Leaflet.AnimatedSearchBox.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<title>Account Managment</title>
 </head>
 <body>
 	<div class="scrollmenu">
@@ -46,15 +35,13 @@
 							</div>
 
 							<div class="inputDiv">
-								<label class="inputLabel" for="confirmPassword"
-									>Confirm Password</label
-								>
+								<label class="inputLabel" for="confirmPassword">Confirm Password</label>
 								<input class="changeInput" type="password" id="cnp" 	name="confirmPassword" />
 							</div>
 
 							<div class="buttonWrapper">
 								<button
-									type="passSubmit"
+									type="button"
 									id="passSubmitButton"
 									onclick="cPass()"
 									class="submitButton"
@@ -79,7 +66,7 @@
 
 							<div class="buttonWrapper">
 							  	<button
-									type="nameSubmit"
+									type="button"
 									id="nameSubmitButton"
 									onclick="cName()"
 									class="submitButton"
@@ -92,12 +79,39 @@
 				</div>
 			</div>
 		</div>
+
+		<div class="mainDiv">
+			<div class="cardStyle">
+				<form name="makeAdminForm" id="makeAdminForm">
+					<h2 class="formTitle">Power of god: Turn a User into an Admin</h2>
+
+					<div class="inputDiv">
+						<label class="inputLabel" for="usermakeAdminname">Select User</label>
+						<select id="makeAdmin" class="changeInput" aria-label="Default select example">
+							<option selected>Choose User</option>
+						</select>
+					</div>
+
+					<div class="buttonWrapper">
+						<button
+							type="button"
+							id="makeAdminSubmit"
+							class="submitButton"
+						>
+						Submit
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
 	</div>
+
+
 	<!-- Footer -->
 	<?php include "footer.php";?>
 	<script>
 		function cName() {
-			const newn = document.getElementById("nu").value;
+			let newn = document.getElementById("nu").value;
 
 			if (newn == "") {
 				alert("Please enter the new Username");
@@ -134,6 +148,7 @@
 
 			function success(result) {
 				if (result == 0) {
+					document.getElementById("nu").value = "";
 					alert("Your Username has been updated successfully");
 				}
 			}
@@ -192,6 +207,9 @@
 
 			function success(res) {
 				if (res == 0) {
+					document.getElementById("op").value = '';
+					document.getElementById("np").value = '';
+					document.getElementById("cnp").value = '';
 					alert("Your password has been updated successfully");
 				} else if (res == 1) {
 					alert("Incorrect password");
@@ -199,6 +217,54 @@
 					alert("An unexpected error has occurred");
 				}
 			}
+		}
+
+
+		window.onload = () => {
+			
+			const getUsers = $.ajax({
+				url: 'powerOfGodSelect.php',
+				method: 'GET',
+				dataType: 'json',
+				success: function(data){
+					// console.log(data)
+				}
+			})
+			
+			getUsers.done(people)
+			
+			function people(result){
+				result.map((person) => {
+					// console.log(person)
+					let option = document.createElement("option");
+					option.value = person.User_id;
+					option.text = person.Username
+					document.getElementById("makeAdmin").appendChild(option);
+				})
+				
+				document.getElementById('makeAdminSubmit').onclick = function(){
+					let select = document.getElementById("makeAdmin");
+					if(select.value === "Choose User"){
+						alert('Error, Please select a User!')
+					}else{
+						const updateUser = $.ajax({
+							url: 'userUpdate.php',
+							method: 'POST',
+							dataType: 'json',
+							data:{userId: select.value},
+							success: function(data){
+								console.log(data)
+								if(data === 0){
+									alert('Success')
+									select.value='Choose User'
+								}
+							}
+						})
+					}
+					console.log(select.value)
+				}
+
+			}			
 		}
 
 	</script> 
