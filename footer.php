a1e08a37e3a590b6fbb07f117756f52908892c35
Xristos9 <sonic9495@gmail.com>
2022-09-01 15:18:34 +0300

    remove crap

--- footer.php
+++ footer.php
@@ -8,14 +8,6 @@
 				<span><i class="fa fa-fw fa-phone">&nbsp;&nbsp;6969696969</i></span>
 				<br>
 				<span><i class="fa fa-fw fa-envelope">&nbsp;ceid@upatras.gr</i></span>
-				<br>
-
-				<h4>Follow us on social media</h4>
-
-				<a href="https://www.youtube.com/watch?v=h3RNElMbdUY" target="_blank"><i class="fa fa-fw fa-facebook"></i></a>
-				<a href="https://www.youtube.com/watch?v=h3RNElMbdUY" target="_blank"><i class="fa fa-fw fa-instagram"></i></a>
-				<a href="https://www.youtube.com/watch?v=h3RNElMbdUY" target="_blank"><i class="fa fa-fw fa-twitter"></i></a>
-				<a href="https://www.youtube.com/watch?v=h3RNElMbdUY" target="_blank"><i class="fa fa-fw fa-youtube"></i></a>
 			</div>
 		</div>
 
