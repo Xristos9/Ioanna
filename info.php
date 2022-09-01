db5f85d4c4e06de847fccae1f91e3c2c8ed296df
Xristos9 <sonic9495@gmail.com>
2022-09-01 15:41:40 +0300

    title change

--- info.php
+++ info.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-	<title>Account info</title>
+	<title>Account Managment</title>
 </head>
 <body>
 	<div class="scrollmenu">
